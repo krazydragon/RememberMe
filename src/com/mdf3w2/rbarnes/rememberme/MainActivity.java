9fd6f7491e30ed2b35e661e21951a5c98824647c
Ronaldo Barnes <krazydragon@fullsail.com>
2013-04-17 07:29:55 -0700

    detect location setup

--- src/com/mdf3w2/rbarnes/rememberme/MainActivity.java
+++ src/com/mdf3w2/rbarnes/rememberme/MainActivity.java
@@ -11,7 +11,12 @@ package com.mdf3w2.rbarnes.rememberme;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -19,8 +24,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener{
+	
+	private LocationManager locationManager;
+	private String provider;
 	private static final int IMAGE_CAPTURE = 0;
     private Button startBtn;
     private Uri imageUri;
@@ -38,6 +47,22 @@ public class MainActivity extends Activity {
                 startCamera();
             }
         });
+        
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+          System.out.println("Provider " + provider + " has been selected.");
+          onLocationChanged(location);
+        } else {
+          
+        }
     }
 
     public void startCamera() {
@@ -58,9 +83,52 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == IMAGE_CAPTURE) {
             if (resultCode == RESULT_OK){
-                Log.d("CAMERA","Picture taken!!!");
+                Log.i("CAMERA","Picture taken!!!");
                 imageView.setImageURI(imageUri);
             }
         }
+    }
+    
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+      super.onResume();
+      locationManager.requestLocationUpdates(provider, 0, 0, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+      super.onPause();
+      locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+      double lat = (double) (location.getLatitude());
+      double lng = (double) (location.getLongitude());
+      
+      Log.i("LATITUDE",String.valueOf(lat));
+      Log.i("LATITUDE",String.valueOf(lng));
+      ;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+      // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+      Toast.makeText(this, "Enabled new provider " + provider,
+          Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+      Toast.makeText(this, "Disabled provider " + provider,
+          Toast.LENGTH_SHORT).show();
     }
 }
